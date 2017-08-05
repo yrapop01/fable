b4db9703448bc8b7fc5532b27e41ff82959c2108
Yuri Rapoport <jerry@Yuris-Air.home>
2017-08-05 06:12:11 +0300

    remove css to a separeate file

--- fable/front/js/notebook.js
+++ fable/front/js/notebook.js
@@ -3,93 +3,42 @@
  */
 
 /*
- * CELL CSS & HTML TEMPLATE. 
+ * CELL HTML TEMPLATE. 
  */
-let CELL_CSS = "<style type='text/css'>\n" +
-               ".editor-wrapper { " +
-                    "border-radius: 0 !important;" +
-                    "margin: 0 !important;" +
-                    "background-color: rgba(214,219,191,1) !important;" +
-               " }\n" +
-               ".editor { " +
-                    "font-size: normal !important;" +
-                    //"border: 1px solid;" +
-                    //*"border-color: #e7e8c1;" +
-                    "background-color: rgba(254,255,232,1) !important;" +
-               " }\n" +
-               "div.output { " +
-                    "border-top-left-radius: 0px !important; border-top-right-radius: 0 !important;" +
-                    "border-bottom-left-radius: 0 !important; border-bottom-right-radius: 0 !important;" +
-                    "margin-top: 0 !important;" +
-                    "margin-bottom: 0 !important;" +
-                    "max-height: 100vh;" +
-                    "overflow: scroll;" +
-               " }\n" +
-               "div.output { " +
-                    "background-color: rgba(254,255,232,1) !important;" +
-               " }\n" +
-               ".hidden { " +
-                    "display: none !important;" +
-               " }\n" +
-               ".meta-wrapper { " +
-                    "font-size: x-small !important;" +
-                    "display: flex;" +
-                    "flex-direction: row;" +
-                    "justify-content: space-between;" +
-                    "border: 1px solid;" +
-                    "border-color: silver;" +
-                    "align-items: center;" +
-               " }\n" +
-               ".info-value { " +
-                    "padding-right: 1em;" +
-               " }\n" +
-               ".info-value, .info-label { " +
-                    "opacity: 0.8;" +
-               " }\n" +
-               ".btn-xs { " +
-                    "padding-top: 0px !important;" +
-                    "padding-bottom: 0px !important;" +
-               " }\n" +
-               "div.top { " +
-                    "margin-bottom: 0px !important;" +
-               " }\n" + 
-               "</style>";
 
-let CELL_HTML = "<div class='row top' id='top-{ID}'><div class='col-xs-12'>" +
+let CELL_HTML = "<div class='row cell-top' id='top-{ID}'><div class='col-xs-12'>" +
 
-                "<pre id='editor-wrapper-{ID}' class='editor-wrapper'>" +
-                "<code class='code editor' contenteditable='true' id='editor-{ID}' spellcheck='false'></code>" +
+                "<pre id='editor-wrapper-{ID}' class='cell-editor-wrapper'>" +
+                "<code class='cell-editor' contenteditable='true' id='editor-{ID}' spellcheck='false'></code>" +
                 "</pre>" +
 
-                "<!--div class='editor-wrapper_1'><div class='editor-wrapper_2'><div class='code editor' id='editor-{ID}'></div></div></div-->" +
+                "<div id='output-{ID}' class='cell-output hidden'></div>" +
 
-                "<div id='output-{ID}' class='well well-sm output hidden'></div>" +
-
-                "<div class='meta-wrapper controls-bg'>" +
-                    "<span class='meta' id='controls-wrapper-{ID}'>" +
-                        '<a id="select-{ID}" class="btn btn-xs btn-outline-primary">' +
+                "<div class='cell-meta-wrapper notebook-controls'>" +
+                    "<span id='controls-wrapper-{ID}'>" +
+                        '<a id="select-{ID}" class="cell-button">' +
                             '<span class="glyphicon glyphicon-unchecked"></span>' +
                             '<span class="glyphicon glyphicon-check hidden"></span>' +
                         '</a>' +
-                        '<a id="toggle-{ID}" class="btn btn-xs btn-outline-primary">' +
+                        '<a id="toggle-{ID}" class="cell-button">' +
                             '<span class="glyphicon glyphicon-eye-close"></span>' +
                             '<span class="glyphicon glyphicon-eye-open hidden"></span>' +
                         '</a>' +
-                        '<a id="clear-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-erase"></span></a>' +
-                        '<a id="up-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-chevron-up"></span></a>' +
-                        '<a id="down-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-chevron-down"></span></a>' +
-                        '<a id="add-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-plus"></span></a>' +
-                        '<a id="del-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-trash"></span></a>' +
-                        '<a id="stop-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-stop"></span></a>' +
-                        '<a id="start-{ID}" class="btn btn-xs btn-outline-primary"><span class="glyphicon glyphicon-play"></span></a>' +
+                        '<a id="clear-{ID}" class="cell-button"><span class="glyphicon glyphicon-erase"></span></a>' +
+                        '<a id="up-{ID}" class="cell-button"><span class="glyphicon glyphicon-chevron-up"></span></a>' +
+                        '<a id="down-{ID}" class="cell-button"><span class="glyphicon glyphicon-chevron-down"></span></a>' +
+                        '<a id="add-{ID}" class="cell-button"><span class="glyphicon glyphicon-plus"></span></a>' +
+                        '<a id="del-{ID}" class="cell-button"><span class="glyphicon glyphicon-trash"></span></a>' +
+                        '<a id="stop-{ID}" class="cell-button"><span class="glyphicon glyphicon-stop"></span></a>' +
+                        '<a id="start-{ID}" class="cell-button"><span class="glyphicon glyphicon-play"></span></a>' +
                     "</span>" +
-                    "<span class='meta' id='info-{ID}'>" +
-                        "<span class='info-label'>Item: </span>" +
-                        "<span class='info-value' id='item-{ID}'>0</span>" +
-                        "<span class='info-label'>Order: </span>" +
-                        "<span class='info-value' id='order-{ID}'>0</span>" +
-                        "<span class='info-label'>State: </span>" +
-                        "<span class='info-value' id='state-{ID}'>Idle</span>" +
+                    "<span id='info-{ID}'>" +
+                        "<span class='cell-info-label'>Item: </span>" +
+                        "<span class='cell-info-value' id='item-{ID}'>0</span>" +
+                        "<span class='cell-info-label'>Order: </span>" +
+                        "<span class='cell-info-value' id='order-{ID}'>0</span>" +
+                        "<span class='cell-info-label'>State: </span>" +
+                        "<span class='cell-info-value' id='state-{ID}'>Idle</span>" +
                     "</span>" +
                 "</div>" +
 
@@ -312,8 +261,6 @@ function Notebook(container) {
         self.cells =  {};
         self.container.innerHTML = '';
     }
-
-    self.container.insertAdjacentHTML('beforeBegin', CELL_CSS);
 
     return self;
 }
